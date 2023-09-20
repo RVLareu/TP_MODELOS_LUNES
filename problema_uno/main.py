54596d6de80dc0db32c746a32f2119fd69756d7f
romanvazquez <roman.vazquez@cardinalsisa.com>
2023-09-20 14:57:44 -0300

    Entrega 2.

--- problema_uno/main.py
+++ problema_uno/main.py
@@ -1,12 +1,28 @@
-sucursales = {}
-capacidad = -1
-dimension = -1
 soluciones = []
+CANTIDAD_MAX_ITER = 5000
+cantidad_iter = 0
+import math
 
-DISTANCIA_RECORRIDA_ORIGEN = 0
+def distancia_entre_cada_una_de_las_sucursales(sucursales):
+    """
+    Devuelve una matriz con la distancia entre cada una de las sucursales.
+    """
+    matriz_distancias = []
+    for sucursal_uno in sucursales.items():
+        matriz_distancias.append([])
+        for sucursal_dos in sucursales.items():
+            matriz_distancias[-1].append(distancia_entre_sucursales(sucursal_uno[1]["posicion_x"], sucursal_uno[1]["posicion_y"], sucursal_dos[1]["posicion_x"], sucursal_dos[1]["posicion_y"]))
+    return matriz_distancias
+    
 
 
 def main():
+
+    sucursales = {}
+    capacidad = -1
+    dimension = -1
+
+    DISTANCIA_RECORRIDA_ORIGEN = 0
     with open("primer_problema.txt", "r") as file:
         line = file.readline()
         while line:
@@ -30,51 +46,72 @@ def main():
                     
                     posicion_x = float(line.split(" ")[1])
                     posicion_y = float(line.split(" ")[2])
-                    sucursales[numero_sucursal]["posicion_x"] = posicion_x
-                    sucursales[numero_sucursal]["posicion_y"] = posicion_y
+                    sucursales[numero_sucursal]["posicion_x"] = math.trunc(posicion_x)
+                    sucursales[numero_sucursal]["posicion_y"] = math.trunc(posicion_y)
                     line = file.readline()
             line = file.readline()
+    
+    matriz_distancias = distancia_entre_cada_una_de_las_sucursales(sucursales)
+
     for sucursal in sucursales.items():
         if (sucursal[1]["demanda"] >= 0 and sucursal[1]["demanda"] <= capacidad):
-            resolver(sucursal[1]["demanda"], capacidad, sucursales, [], DISTANCIA_RECORRIDA_ORIGEN, sucursal)
-
+            print("Sucursal origen: ", sucursal[0])
+            # reinicio cantidad iter para cada sucursal origen
+            global cantidad_iter
+            cantidad_iter = 0
+            resolver(sucursal[1]["demanda"], capacidad, sucursales, [], DISTANCIA_RECORRIDA_ORIGEN, sucursal, matriz_distancias)
 
     soluciones.sort(key=lambda x: x[1])
     with open("solucion.txt", "w") as file:
         # escribir sucursales según orden de visita separando por espacio
         file.write(" ".join([str(x[0]) for x in soluciones[0][0]]))
+        print(soluciones[0][1])
 
 
-def sucursales_validas(carga_actual, capacidad, sucursales, sucursales_visitadas):
+
+
+def sucursal_valida(carga_actual, capacidad, sucursales, sucursales_visitadas, sucursal_actual, matriz_distancias):
     """
-    Devuelve una lista de sucursales válidas. 
-    Una sucursal es valida si su demanda no rompre la restriccion de capacidad y si no fue visitada anteriormente.
+    Devuelve las n sucursales mas cercanas validas a la actual.
+    Valida es que cumpla las restricciones de capacidad y demanda.
     """
     sucursales_validas = []
     for sucursal in sucursales.items():
-        if sucursal[0] not in [x[0] for x in sucursales_visitadas] and carga_actual + sucursal[1]["demanda"] <= capacidad and carga_actual + sucursal[1]["demanda"] >= 0:
+        if sucursal[0] not in [x[0] for x in sucursales_visitadas] and sucursal[1]["demanda"] + carga_actual <= capacidad and sucursal[1]["demanda"] + carga_actual >= 0:
             sucursales_validas.append(sucursal)
-    return sucursales_validas
+    sucursales_validas.sort(key=lambda x: matriz_distancias[sucursal_actual[0] - 1][x[0] - 1])
+    return sucursales_validas[:2]
+    
+
 
 def distancia_entre_sucursales(poxicion_x_sucursal_uno, posicion_y_sucursal_uno, posicion_x_sucursal_dos, posicion_y_sucursal_dos):
-    return ((poxicion_x_sucursal_uno - posicion_x_sucursal_dos)**2 + (posicion_y_sucursal_uno - posicion_y_sucursal_dos)**2)**(1/2)
+    return math.trunc(((poxicion_x_sucursal_uno - posicion_x_sucursal_dos)**2 + (posicion_y_sucursal_uno - posicion_y_sucursal_dos)**2)**(1/2))
 
-def resolver(carga_actual, capacidad, sucursales, sucursales_visitadas, distancia_recorrida, sucursal_actual):
+def resolver(carga_actual, capacidad, sucursales, sucursales_visitadas, distancia_recorrida, sucursal_actual, matriz_distancias):
     sucursales_visitadas.append(sucursal_actual)
+    global cantidad_iter
+    cantidad_iter += 1
+    if cantidad_iter > CANTIDAD_MAX_ITER:
+        return
+    if (distancia_recorrida > 8700):
+        return False
     if len(sucursales_visitadas) == len(sucursales):
-        soluciones.append((sucursales_visitadas, distancia_recorrida))
-        return True
-    proximas_sucursales_validas = sucursales_validas(carga_actual, capacidad, sucursales, sucursales_visitadas)
-    for sucursal in proximas_sucursales_validas:
-        distancia_recorrida += distancia_entre_sucursales(sucursales[sucursal_actual[0]]["posicion_x"], sucursales[sucursal_actual[0]]["posicion_y"], sucursales[sucursal[0]]["posicion_x"], sucursales[sucursal[0]]["posicion_y"])
-        carga_actual += sucursales[sucursal[0]]["demanda"]
-        if resolver(carga_actual, capacidad, sucursales, sucursales_visitadas, distancia_recorrida, sucursal):
+        # Me quedo con la mejor socución
+        soluciones.append((sucursales_visitadas.copy(), distancia_recorrida))
+        return
+    sucursales_validas = sucursal_valida(carga_actual, capacidad, sucursales, sucursales_visitadas, sucursal_actual, matriz_distancias)
+    if sucursales_validas is None:
+        return False
+    for prox_sucursal in sucursales_validas:
+        distancia_recorrida += matriz_distancias[sucursal_actual[0] - 1][prox_sucursal[0] - 1]
+        carga_actual += sucursales[prox_sucursal[0]]["demanda"]
+        if resolver(carga_actual, capacidad, sucursales, sucursales_visitadas, distancia_recorrida, prox_sucursal, matriz_distancias):
             return True
         else:
-            print([x[0] for x in sucursales_visitadas])
-            distancia_recorrida -= distancia_entre_sucursales(sucursales[sucursal_actual[0]]["posicion_x"], sucursales[sucursal_actual[0]]["posicion_y"], sucursales[sucursal[0]]["posicion_x"], sucursales[sucursal[0]]["posicion_y"])
-            carga_actual -= sucursales[sucursal[0]]["demanda"]
+            distancia_recorrida -= matriz_distancias[sucursal_actual[0] - 1][prox_sucursal[0] - 1]
+            carga_actual -= sucursales[prox_sucursal[0]]["demanda"]
             sucursales_visitadas.pop()
+    
     return False
 
 if __name__ == "__main__":
